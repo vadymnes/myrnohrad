e7c3222d27ef95dd85deb553778aa3d18aa05180
vadymnes <60113016+vadymnes@users.noreply.github.com>
2021-11-02 13:54:45 +0200

    вернув карту назад

--- js/main.js
+++ js/main.js
@@ -8,30 +8,20 @@ var default_zoom = window.innerWidth < 813 ? 13 : 14;
 var map = L.map('map').setView(default_coordinates, 11);
 
 
-// L.tileLayer(
-//     // 'https://api.mapbox.com/styles/v1/evgeshadrozdova/ckl1654or031r17mvc4wr7edc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXZnZXNoYWRyb3pkb3ZhIiwiYSI6ImNqMjZuaGpkYTAwMXAzMm5zdGVvZ2c0OHYifQ.s8MMs2wW15ZyUfDhTS_cdQ',
+L.tileLayer(
+    // 'https://api.mapbox.com/styles/v1/evgeshadrozdova/ckl1654or031r17mvc4wr7edc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXZnZXNoYWRyb3pkb3ZhIiwiYSI6ImNqMjZuaGpkYTAwMXAzMm5zdGVvZ2c0OHYifQ.s8MMs2wW15ZyUfDhTS_cdQ',
 
-//     //красива, але у stamen недекомунізовані вулиці
-//     //'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}',
+    //красива, але у stamen недекомунізовані вулиці
+    //'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}',
 
-//     //резервна підложка, якщо mapbox вибере ліміт
-//     'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-//     {
-//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-//         subdomains: 'abcd',
-//         maxZoom: 19,
-//         ext: 'png'
-//     }).addTo(map);
-
-
-L.tileLayer('https://api.mapbox.com/styles/v1/vadenvmapkyiv/ckebblhya13tx1at7q6wpso5m.html?title=view&access_token=pk.eyJ1IjoidmFkZW52bWFwa3lpdiIsImEiOiJja2RwczBzcXYwMjgzMnprdWVxMTB2aWU0In0.OBDKIqeo6YDnxaEZR4bdIA', {
-attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
-tileSize: 512,
-maxZoom: 18,
-zoomOffset: -1,
-id: 'mapbox/streets-v11',
-accessToken: 'pk.eyJ1IjoidmFkZW52bWFwa3lpdiIsImEiOiJja2RwczBzcXYwMjgzMnprdWVxMTB2aWU0In0.OBDKIqeo6YDnxaEZR4bdIA'
-}).addTo(map);
+    //резервна підложка, якщо mapbox вибере ліміт
+    'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19,
+        ext: 'png'
+    }).addTo(map);
 
 map.scrollWheelZoom.disable();
 var canvasRenderer = L.canvas();
